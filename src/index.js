d1c1815312dd00bc87dd60b7853589120f990e96
magdiszilagyi <41055397+magdiszilagyi@users.noreply.github.com>
2018-07-10 14:32:04 +0200

    Create SerachBar with input
    on  change assign value to state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyCBrm8ce4APVNr_A4KBdSxFbtlzRc7IxOY";
 
 // create a new component. This component should pproduce some HTML
 
-const App = () => { //class
-  return <div>HI!</div>
-}
+const App = () => {
+  //class
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>//instance
-, document.querySelector('.container'));
+ReactDOM.render(
+  <App />, //instance
+  document.querySelector(".container")
+);
