5cb7bc52876b6aad26573f36a1969a9e910f9cf5
magdiszilagyi <41055397+magdiszilagyi@users.noreply.github.com>
2018-07-11 09:39:10 +0200

    Assign state to input value in SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,13 +4,14 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: "" };
+    this.state = { term: "start" };
   }
 
   render() {
     return (
       <div>
         <input
+          value={this.state.term}
           onChange={event => this.setState({ term: event.target.value })}
         />
       </div>
