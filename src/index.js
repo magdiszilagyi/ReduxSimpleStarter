c3964ddd5e075cd49c4b062633cf70da09e3d8a0
magdiszilagyi <41055397+magdiszilagyi@users.noreply.github.com>
2018-07-17 13:06:39 +0200

    Search videos by input

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: "surfboards" }, data => {
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, data => {
       this.setState({
         videos: data,
         selectedVideo: data[0]
@@ -27,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
